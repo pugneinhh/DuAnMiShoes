5829491ffcf3ab3f337b9718ffe080c2c0e653a3
anhntpph28990@fpt.edu.vn <anhntpph28990@fpt.edu.vn>
2023-11-16 21:39:52 +0700

    đặt lịch voucher

--- src/main/java/com/example/duanmishoes/DuAnMiShoesApplication.java
+++ src/main/java/com/example/duanmishoes/DuAnMiShoesApplication.java
@@ -2,8 +2,10 @@ package com.example.duanmishoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DuAnMiShoesApplication {
 
     public static void main(String[] args) {
